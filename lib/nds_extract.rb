4011d190a7f3e76a001887973ff99a73a98416f0
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-09 13:51:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  
 
 end
 
